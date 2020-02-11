d9a9fecb7381649c6098edfcf9cbe320146b8680
ReeceASharp <ReeceA.Sharp@gmail.com>
2020-02-11 16:54:22 -0700

    help

--- src/main/java/cs455/overlay/node/Node.java
+++ src/main/java/cs455/overlay/node/Node.java
@@ -1,7 +1,9 @@
 package cs455.overlay.node;
 
 import cs455.overlay.wireformats.Event;
+import cs455.overlay.wireformats.EventFactory;
 
 public interface Node {
 	public void onEvent(Event e);
+	public EventFactory getFactory();
 }
